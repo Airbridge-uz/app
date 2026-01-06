dbe9379c05391ad0e753136bd57fbaf287cdf4e3
khaitbek <khaitbek2005@gmail.com>
2026-01-06 23:12:37 +0500

    better classnames for readability

--- src/shared/ui/tabs.tsx
+++ src/shared/ui/tabs.tsx
@@ -22,7 +22,12 @@ function Tabs({
 }
 
 const tabsListVariants = cva(
-  "rounded-lg p-[3px] group-data-horizontal/tabs:h-8 data-[variant=line]:rounded-none group/tabs-list text-muted-foreground inline-flex w-fit items-center justify-center group-data-[orientation=vertical]/tabs:h-fit group-data-[orientation=vertical]/tabs:flex-col",
+  [
+    "rounded-lg p-0.75 text-muted-foreground inline-flex w-fit items-center justify-center",
+    "group-data-[orientation=horizontal]/tabs:h-8",
+    "group-data-[orientation=vertical]/tabs:h-fit group-data-[orientation=vertical]/tabs:flex-col",
+    "data-[variant=line]:rounded-none group/tabs-list",
+  ].join(" "),
   {
     variants: {
       variant: {
@@ -77,4 +82,5 @@ function TabsContent({ className, ...props }: TabsPrimitive.Panel.Props) {
   )
 }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent, tabsListVariants }
+export { Tabs, TabsContent, TabsList, tabsListVariants, TabsTrigger }
+
